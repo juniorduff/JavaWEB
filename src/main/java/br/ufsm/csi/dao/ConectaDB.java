1091f96150fdd8bd5541e49143ce7f606779f366
Junior <junior_duff_sm@hotmail.com>
2020-04-08 19:02:46 -0300

    web java

---
+++ src/main/java/br/ufsm/csi/dao/ConectaDB.java
@@ -0,0 +1,36 @@
+package br.ufsm.csi.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ConectaDB {
+
+    public static  void main(String args[]){
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn =
+                    DriverManager.getConnection("jdbc:postgresql://localhost:5432/CODVID19",
+                            "postgres","postgres");
+            System.out.println("conexao realizada com sucesso");
+
+            // Instanciando o objeto statement (stmt)
+            Statement stmt = conn.createStatement();
+       ResultSet rs = stmt.executeQuery("select * from usuario");
+
+        while (rs.next()){
+            System.out.println("id: "+ rs.getInt("id_usuario"));
+            System.out.println("nome: "+rs.getString("nome"));
+            System.out.println("email: "+rs.getString("email"));
+        }
+
+        } catch (Exception e){
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
---
+++ src/main/java/br/ufsm/csi/model/Usuario.java
@@ -0,0 +1,62 @@
+package br.ufsm.csi.model;
+
+import java.sql.Date;
+public class Usuario {
+    private int id;
+    private String nome;
+    private String email;
+    private String senha;
+    private Date data_cadastro;
+    private boolean ativo;
+
+    public Usuario() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Date getData_cadastro() {
+        return data_cadastro;
+    }
+
+    public void setData_cadastro(Date data_cadastro) {
+        this.data_cadastro = data_cadastro;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+}
